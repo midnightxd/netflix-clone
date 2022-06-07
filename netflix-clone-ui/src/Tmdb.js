69258b49c5e927fb47974ffe1b7a2f01949cd591
midnightxd <leonardigit@gmail.com>
2022-06-07 15:02:34 -0300

    Add api

--- netflix-clone-ui/src/Tmdb.js
+++ netflix-clone-ui/src/Tmdb.js
@@ -51,5 +51,24 @@ export default {
         items: await basicFetch(`/discover/movie?with_generes=99&language=pt-BR&api_key=${API_KEY}`)
       }
     ];
+  },
+  getMovieInfo: async (movieID, type) => {
+    let info = {}
+
+      if(movieID) {
+        switch(type) {
+          case 'movie':
+            info = await basicFetch(`/movie/${movieID}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+          case 'tv':
+            info = await basicFetch(`/tv/${movieID}?language=pt-BR&api_key=${API_KEY}`);
+          break;
+          default:
+            info = null;
+          break
+        }
+      }
+
+      return info;
   }
 }
